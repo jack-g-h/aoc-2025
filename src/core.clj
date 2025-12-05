413b38044daefe8b8bd5b81664383ef7d8737094
Jack Holt <jack.g.holt@gmail.com>
2025-12-04 23:43:15 -0600

    Renames parsers in runner comment blocks.
    Allows for multiple days to be run without redefining parser.

--- src/core.clj
+++ src/core.clj
@@ -13,62 +13,60 @@
 
 ;;; Run Day 1
 (comment
-  (def parser (day-parser "day-1" day-1/parse-move-sequence))
-  (parser "example")
+  (def parser-1 (day-parser "day-1" day-1/parse-move-sequence))
+  (parser-1 "example")
 
-  (day-1/solve-part-1 (parser "example"))
-  (day-1/solve-part-1 (parser "actual"))
+  (day-1/solve-part-1 (parser-1 "example"))
+  (day-1/solve-part-1 (parser-1 "actual"))
 
-  (day-1/solve-part-2 (parser "example"))
-  (day-1/solve-part-2 (parser "actual"))
+  (day-1/solve-part-2 (parser-1 "example"))
+  (day-1/solve-part-2 (parser-1 "actual"))
   ,)
 
 ;;; Run Day 2
 (comment
-  (def parser (day-parser "day-2" day-2/parse-ranges))
-  (parser "example")
+  (def parser-2 (day-parser "day-2" day-2/parse-ranges))
+  (parser-2 "example")
 
-  (mapv day-2/invalid-number? (reduce into (parser "example")))
+  (day-2/solve-part-1 (parser-2 "example"))
+  (day-2/solve-part-1 (parser-2 "actual"))
 
-  (day-2/solve-part-1 (parser "example"))
-  (day-2/solve-part-1 (parser "actual"))
-
-  (day-2/solve-part-2 (parser "example"))
-  (day-2/solve-part-2 (parser "actual"))
+  (day-2/solve-part-2 (parser-2 "example"))
+  (day-2/solve-part-2 (parser-2 "actual"))
   ,)
 
 ;;; Run Day 3
 (comment
-  (def parser (day-parser "day-3" day-3/parse-batteries))
-  (parser "example")
+  (def parser-3 (day-parser "day-3" day-3/parse-batteries))
+  (parser-3 "example")
 
-  (day-3/solve-part-1 (parser "example"))
-  (day-3/solve-part-1 (parser "actual"))
+  (day-3/solve-part-1 (parser-3 "example"))
+  (day-3/solve-part-1 (parser-3 "actual"))
 
-  (day-3/solve-part-2 (parser "example"))
-  (day-3/solve-part-2 (parser "actual"))
+  (day-3/solve-part-2 (parser-3 "example"))
+  (day-3/solve-part-2 (parser-3 "actual"))
   ,)
 
 ;;; Run Day 4
 (comment
-  (def parser (day-parser "day-4" day-4/parse-rolls))
-  (parser "example")
+  (def parser-4 (day-parser "day-4" day-4/parse-rolls))
+  (parser-4 "example")
 
-  (day-4/solve-part-1 (parser "example"))
-  (day-4/solve-part-1 (parser "actual"))
+  (day-4/solve-part-1 (parser-4 "example"))
+  (day-4/solve-part-1 (parser-4 "actual"))
 
-  (day-4/solve-part-2 (parser "example"))
-  (day-4/solve-part-2 (parser "actual"))
+  (day-4/solve-part-2 (parser-4 "example"))
+  (day-4/solve-part-2 (parser-4 "actual"))
   ,)
 
 ;;; Run Day 5
 (comment
-  (def parser (day-parser "day-5" day-5/parse-database))
-  (parser "example")
+  (def parser-5 (day-parser "day-5" day-5/parse-database))
+  (parser-5 "example")
 
-  (day-5/solve-part-1 (parser "example"))
-  (day-5/solve-part-1 (parser "actual"))
+  (day-5/solve-part-1 (parser-5 "example"))
+  (day-5/solve-part-1 (parser-5 "actual"))
 
-  (day-5/solve-part-2 (parser "example"))
-  (day-5/solve-part-2 (parser "actual"))
+  (day-5/solve-part-2 (parser-5 "example"))
+  (day-5/solve-part-2 (parser-5 "actual"))
   ,)
